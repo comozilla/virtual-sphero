a8613a3d98632a92494b06719eb7c4a264f26f33
nakamurataichi <ringo.m3@icloud.com>
2016-07-29 12:53:23 +0900

    matter.jsに変更

--- virtual/main.js
+++ virtual/main.js
@@ -1,3 +1,9 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Body = Matter.Body,
+    Bodies = Matter.Bodies;
+
 var VirtualSpheroController = (function() {
   var SpeedController = (function() {
     function SpeedController() {
@@ -28,51 +34,36 @@ var VirtualSpheroController = (function() {
     this.socket.on("command", (commandName, args) => {
       Object.keys(this.virtualSpheros).forEach(virtualSpheroName => {
         var virtualSphero = this.virtualSpheros[virtualSpheroName];
-        if (typeof virtualSphero[commandName] !== "undefined") {
-          virtualSphero[commandName].apply(virtualSphero, args);
+        if (commandName === "roll") {
+          Matter.Body.applyForce(virtualSphero, { x: 0, y: 1}, { x: 0, y: -0.1 });
         }
+        // if (typeof virtualSphero[commandName] !== "undefined") {
+        //   virtualSphero[commandName].apply(virtualSphero, args);
+        // }
       });
     });
 
     this.speedController = new SpeedController();
+
+    this.engine = Engine.create();
+    this.engine.world.gravity.y = 0;
+
     this.canvas = document.getElementById("canvas");
     this.ctx = this.canvas.getContext("2d");
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
 
+    var ground1 = Bodies.rectangle(400, 590, 800, 20, { isStatic: true });
+    var ground2 = Bodies.rectangle(10, 300, 20, 600, { isStatic: true });
+    var ground3 = Bodies.rectangle(400, 10, 800, 20, { isStatic: true });
+    var ground4 = Bodies.rectangle(790, 300, 20, 600, { isStatic: true });
+
+    World.add(this.engine.world, [ground1, ground2, ground3, ground4]);
+    Engine.run(this.engine);
+
     var tick = () => {
       this.clearCanvas();
-      var moveSphero = [];
-      Object.keys(this.virtualSpheros).forEach((virtualSpheroName, index) => {
-        var virtualSphero = this.virtualSpheros[virtualSpheroName];
-        var collidedSpheroCount = Object.keys(this.virtualSpheros).filter((targetSpheroName, targetIndex) => {
-          var targetSphero = this.virtualSpheros[targetSpheroName];
-          if (targetIndex === index) {
-            return false;
-          }
-          var movedVirtualSpheroX = virtualSphero.x + virtualSphero.ex;
-          var movedVirtualSpheroY = virtualSphero.y + virtualSphero.ey;
-          var movedTargetSpheroX = targetSphero.x + targetSphero.ex;
-          var movedTargetSpheroY = targetSphero.y + targetSphero.ey;
-          var dx = Math.abs(movedVirtualSpheroX - movedTargetSpheroX);
-          var dy = Math.abs(movedVirtualSpheroY - movedTargetSpheroY);
-          if (Math.sqrt(dx * dx + dy * dy) <= virtualSphero.radius * 2) {
-            var collsionRadian =
-              Math.atan2(movedTargetSpheroY - movedVirtualSpheroY, movedTargetSpheroX - movedVirtualSpheroX) * 180 / Math.PI;
-            var moveRadian = Math.atan2(virtualSphero.ey, virtualSphero.ex) * 180 / Math.PI;
-            return Math.abs(collsionRadian - moveRadian) <= 10;
-          }
-        }).length;
-        if (collidedSpheroCount === 0) {
-          moveSphero.push(index);
-        }
-      });
-      Object.keys(this.virtualSpheros).forEach((virtualSpheroName, index) => {
-        var virtualSphero = this.virtualSpheros[virtualSpheroName];
-        if (moveSphero.indexOf(index) >= 0) {
-          virtualSphero.move();
-        }
-        virtualSphero.draw();
+      Object.keys(this.virtualSpheros).forEach(spheroName => {
       });
       requestAnimationFrame(tick);
     };
@@ -86,7 +77,10 @@ var VirtualSpheroController = (function() {
 
   VirtualSpheroController.prototype.addVirtualSphero = function(spheroName) {
     if (this.showSpheros === null || this.showSpheros.indexOf(spheroName) !== -1) {
-      this.virtualSpheros[spheroName] = new VirtualSphero(this.canvas, this.speedController, spheroName);
+      this.virtualSpheros[spheroName] = Bodies.circle(400, 400, 30, {
+        friction: 0.1
+      });
+      World.add(this.engine.world, [this.virtualSpheros[spheroName]]);
     }
   };
 
@@ -194,3 +188,174 @@ function getParams() {
   });
   return paramsObject;
 }
+=======
+var VirtualSpheroController = (function() {
+  var SpeedController = (function() {
+    function SpeedController() {
+      this._element = document.getElementById("speed");
+      this.speed = 0.2;
+      this._element.value = this.speed;
+      this._element.addEventListener("change", () => {
+        if (this._element.value !== "" && !isNaN(this._element.value)) {
+          this.speed = parseFloat(this._element.value);
+        }
+      });
+    };
+    return SpeedController;
+  })();
+  function VirtualSpheroController() {
+    this.ws = new WebSocket("ws://" + location.host);
+
+    this.ws.onclose = function() {
+        this.ws = null;
+    }.bind(this);
+
+    this.ws.onerror = function(e) {
+        if (typeof errorCallback === "function")
+            errorCallback(e);
+    };
+
+    this.ws.onmessage = function(message) {
+      console.log(message.data);
+      var data;
+      try {
+        data = JSON.parse(message.data);
+      } catch(e) {
+        console.log(e);
+        return;
+      }
+      if (data.command.substring(0, 1) === "_") {
+        console.log(data.command);
+        switch (data.command) {
+          case "_addVirtualSphero":
+            this.addVirtualSphero(data.arguments);
+            break;
+          case "_removeVirtualSphero":
+            this.removeVirtualSphero(data.arguments);
+            break;
+          }
+        } else if(commands.indexOf(data.command) !== -1) {
+        Object.keys(this.virtualSpheros).forEach(virtualSpheroName => {
+          Matter.Body.applyForce(this.virtualSpheros[virtualSpheroName], { x: 0, y: 1}, { x: 0, y: -0.1 });
+        });
+      }
+    }.bind(this);
+
+    this.engine = Engine.create();
+    this.render = Render.create({
+      element: document.getElementById("canvas-container"),
+      engine: this.engine
+    });
+
+    var ground1 = Bodies.rectangle(400, 590, 800, 20, { isStatic: true });
+    var ground2 = Bodies.rectangle(10, 300, 20, 600, { isStatic: true });
+    var ground3 = Bodies.rectangle(400, 10, 800, 20, { isStatic: true });
+    var ground4 = Bodies.rectangle(790, 300, 20, 600, { isStatic: true });
+
+    this.engine.world.gravity.y = 0;
+
+    World.add(this.engine.world, [ground1, ground2, ground3, ground4]);
+
+    Engine.run(this.engine);
+    Render.run(this.render);
+
+    this.virtualSpheros = {};
+  }
+
+  VirtualSpheroController.prototype.addVirtualSphero = function(spheroName) {
+    this.virtualSpheros[spheroName] = Bodies.circle(400, 400, 30, {
+      friction: 0.1
+    });
+    World.add(this.engine.world, [this.virtualSpheros[spheroName]]);
+  };
+
+  VirtualSpheroController.prototype.removeVirtualSphero = function(spheroName) {
+    if (typeof this.virtualSpheros[spheroName] !== "undefined") {
+      delete this.virtualSpheros[spheroName];
+    }
+  };
+
+  var commands = [
+    /* sphero.js */
+    "setHeading",
+    "setStabilization",
+    "setRotationRate",
+    "setCreationDate",
+    "getBallRegWebsite",
+    "reEnableDemo",
+    "getChassisId",
+    "setChassisId",
+    "selfLevel",
+    "setVdl",
+    "setDataStreaming",
+    "setCollisionDetection",
+    "locator",
+    "setAccelerometer",
+    "readLocator",
+    "setRgbLed",
+    "setBackLed",
+    "getRgbLed",
+    "roll",
+    "boost",
+    "move",
+    "setRawMotors",
+    "setMotionTimeout",
+    "setOptionsFlag",
+    "getOptionsFlag",
+    "setTempOptionFlags",
+    "getTempOptionFlags",
+    "getConfigBlock",
+    "setSsbParams",
+    "setDeviceMode",
+    "setConfigBlock",
+    "getDeviceMode",
+    "getSsb",
+    "setSsb",
+    "ssbRefill",
+    "ssbBuy",
+    "ssbUseConsumeable",
+    "ssbGrantCores",
+    "ssbAddXp",
+    "ssbLevelUpAttr",
+    "getPwSeed",
+    "ssbEnableAsync",
+    "runMacro",
+    "saveTempMacro",
+    "saveMacro",
+    "initMacroExecutive",
+    "abortMacro",
+    "macroStatus",
+    "setMacroParam",
+    "appendTempMacroChunk",
+    "eraseOBStorage",
+    "appendOBFragment",
+    "execOBProgram",
+    "abortOBProgram",
+    "answerInput",
+    "commitToFlash",
+    "commitToFlashAlias",
+    /* custom.js */
+    "streamData",
+    "color",
+    "randomColor",
+    "getColor",
+    "detectCollisions",
+    "startCalibration",
+    "finishCalibration",
+    "streamOdometer",
+    "streamVelocity",
+    "streamAccelOne",
+    "streamImuAngles",
+    "streamAccelerometer",
+    "streamGyroscope",
+    "streamMotorsBackEmf",
+    "stopOnDisconnect",
+    "stop"
+  ];
+  return VirtualSpheroController;
+})();
+
+document.addEventListener("DOMContentLoaded", function() {
+  var sphero = new VirtualSpheroController();
+});
+>>>>>>> matter.jsに変更
