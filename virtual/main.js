433a4e0f8bca51986415f9a94504ac05aa8868f3
nakamurataichi <ringo.m3@icloud.com>
2016-08-07 14:45:25 +0900

    cssの読み込みを変更(途中)

--- virtual/main.js
+++ virtual/main.js
@@ -1,4 +1,5 @@
-import VirtualSpheroController from "./virtual-sphero-controller"; 
+import VirtualSpheroController from "./virtual-sphero-controller";
+import "./style.css";
 
 document.addEventListener("DOMContentLoaded", function() {
   const sphero = new VirtualSpheroController();
